201d251f82a61d37009270fb36b17dde903beefd
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2022-09-17 15:42:57 +0300

    fix: изменить форматирование кода, название класса кнопки лайка, переместить папку со шрифтами

--- script/script.js
+++ script/script.js
@@ -1,33 +1,32 @@
-let profileInfo = document.querySelector('.profile');
+let profileInfo = document.querySelector(".profile");
 
-let editButton = profileInfo.querySelector('.profile__edit-button');
-let profileName = profileInfo.querySelector('.profile__name');
-let profileAbout = profileInfo.querySelector('.profile__about');
+let editButton = profileInfo.querySelector(".profile__edit-button");
+let profileName = profileInfo.querySelector(".profile__name");
+let profileAbout = profileInfo.querySelector(".profile__about");
 
-let popUp = document.querySelector('.pop-up');
+let popUp = document.querySelector(".pop-up");
 
-let nameInput = popUp.querySelector('.pop-up__input_change_name');
-let aboutInput = popUp.querySelector('.pop-up__input_change_about');
-let closeButton = popUp.querySelector('.pop-up__close-button');
+let nameInput = popUp.querySelector(".pop-up__input_change_name");
+let aboutInput = popUp.querySelector(".pop-up__input_change_about");
+let closeButton = popUp.querySelector(".pop-up__close-button");
 
 function showPopUp() {
-    popUp.classList.add('pop-up_opened');
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
+  popUp.classList.add("pop-up_opened");
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
-
 function closePopUp() {
-    popUp.classList.remove('pop-up_opened');
+  popUp.classList.remove("pop-up_opened");
 }
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = aboutInput.value;
-    closePopUp();
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  closePopUp();
 }
 
-editButton.addEventListener('click', showPopUp);
-closeButton.addEventListener('click', closePopUp);
-popUp.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener("click", showPopUp);
+closeButton.addEventListener("click", closePopUp);
+popUp.addEventListener("submit", formSubmitHandler);
