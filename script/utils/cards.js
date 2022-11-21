d2fa583da69ba33565072e3c58a2f1efc33217d6
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2022-11-22 00:00:35 +0300

    fix: уменьшен вес картинок

--- script/utils/cards.js
+++ script/utils/cards.js
@@ -1,9 +1,9 @@
-import cupcakesImage from "../../images/card__cupcakes.jpg";
-import deepInnerWorldImage from "../../images/card__deep-inner-world.jpg";
-import purchaseImage from "../../images/card__purchase.jpg";
-import niceDressImage from "../../images/card__nice-dress.jpg";
-import aliceImage from "../../images/card__alice.jpg";
-import workImage from "../../images/card__work.jpg";
+import cupcakesImage from "../../images/card__cupcakes.jpeg";
+import deepInnerWorldImage from "../../images/card__deep-inner-world.jpeg";
+import purchaseImage from "../../images/card__purchase.jpeg";
+import niceDressImage from "../../images/card__nice-dress.jpeg";
+import aliceImage from "../../images/card__alice.jpeg";
+import workImage from "../../images/card__work.jpeg";
 
 export const initialCards = [
   {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "../pages/index.css";
+import "./index.css";
 import { initialCards } from '../script/utils/cards.js';
 import { 
   editButton, 
